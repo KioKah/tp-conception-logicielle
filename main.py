b9a14c2033a51a1ef2645c2f0308e1ffb9bfd366
Guillaume Tarpin-Bernard <guillaume.tarpin-bernard@eleve.ensai.fr>
2024-01-15 15:18:44 +0100

    ajout zonage

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 maintenant = datetime.now()
 heure_formatee = maintenant.strftime("%H:%M:%S")
 print(heure_formatee)
+
+reunion = datetime.now(tz=ZoneInfo("Indian/Reunion"))
+heure_formatee_reunion = reunion.strftime("%H:%M:%S")
+print("Indian/Reunion --", heure_formatee_reunion)
+
+paris = datetime.now(tz=ZoneInfo("Europe/Paris"))
+heure_formatee_paris = reunion.strftime("%H:%M:%S")
+print("Europe/Paris --", heure_formatee_paris)
