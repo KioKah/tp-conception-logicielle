534de0fd3763853965e13b29e6c053a2fae4b8af
Guillaume Tarpin-Bernard <guillaume.tarpin-bernard@eleve.ensai.fr>
2024-01-22 16:26:21 +0100

    Amélioration de main.py (fonction générale et logging)

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from datetime import datetime
-from zoneinfo import ZoneInfo
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
+import logging
 
-maintenant = datetime.now()
-heure_formatee = maintenant.strftime("%H:%M:%S")
-print("Heure locale :", heure_formatee)
+logging.basicConfig(filename="traitement.log", encoding="utf-8", level=logging.DEBUG)
 
-reunion = datetime.now(tz=ZoneInfo("Indian/Reunion"))
-heure_formatee_reunion = reunion.strftime("%H:%M:%S")
-print("Indian/Reunion --", heure_formatee_reunion)
 
-paris = datetime.now(tz=ZoneInfo("Europe/Paris"))
-heure_formatee_paris = paris.strftime("%H:%M:%S")
-print("Europe/Paris --", heure_formatee_paris)
+def afficher_heure(time_zone: str):
+    if time_zone is None:
+        logging.error("Aucune timezone n'a été renseignée")
+        raise ValueError("Aucune timezone n'a été renseignée")
+    logging.debug(f"Demande d'heure sur le timezone : {time_zone}")
+    try:
+        maintenant = datetime.now(tz=ZoneInfo(time_zone))
+        heure_formatee = maintenant.strftime("%H:%M:%S")
+    except ZoneInfoNotFoundError as e:
+        logging.error(f"Erreur : {e}")
+        raise e
+    else:
+        print(f"Heure dans la zone {time_zone} : {heure_formatee}")
+
+
+logging.info(f"Lancement du traitement")
+
+afficher_heure("Indian/Reunion")
+afficher_heure("Europe/Paris")
+afficher_heure("??")
